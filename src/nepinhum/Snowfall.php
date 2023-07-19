ce88fb7c2a318d1dbb9e05bc7815e1956ac7f465
scher470 <120848089+nxpinhum5326@users.noreply.github.com>
2023-07-19 11:45:36 +0300

    Update Snowfall.php

--- src/nepinhum/Snowfall.php
+++ src/nepinhum/Snowfall.php
@@ -7,16 +7,15 @@ use pocketmine\plugin\PluginBase;
 
 class Snowfall extends PluginBase
 {
-    protected static Snowfall $instance;
+    private static Snowfall $instance;
 
 	protected function onLoad(): void
 	{
 		self::$instance = $this;
 	}
-
 	public function onEnable(): void
-    {
-        self::$instance->getServer()->getPluginManager()->registerEvents(new SnowListener(), self::$instance);
+	{
+		self::$instance->getServer()->getPluginManager()->registerEvents(new SnowListener(), self::$instance);
 	}
 
 	public static function getInstance(): Snowfall
