bd30508c93416b9b101cc4f2b68851d3efbbcf94
scher470 <120848089+nxpinhum5326@users.noreply.github.com>
2023-07-19 11:43:52 +0300

    Update and rename Snow.php to Snowfall.php

---
+++ src/nepinhum/Snowfall.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace nepinhum;
+
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+
+class Snowfall extends PluginBase
+{
+    protected static Snowfall $instance;
+
+	protected function onLoad(): void
+	{
+		self::$instance = $this;
+	}
+
+	public function onEnable(): void
+    {
+        self::$instance->getServer()->getPluginManager()->registerEvents(new SnowListener(), self::$instance);
+	}
+
+	public static function getInstance(): Snowfall
+	{
+		return self::$instance;
+	}
+}
