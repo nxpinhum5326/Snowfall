f12a770dbef6908f1655b6cf76f9ce3fe7f56e77
scher470 <120848089+nxpinhum5326@users.noreply.github.com>
2023-07-19 11:44:44 +0300

    Update SnowListener.php

--- src/nepinhum/SnowListener.php
+++ src/nepinhum/SnowListener.php
@@ -2,12 +2,13 @@
 
 namespace nepinhum;
 
+use pocketmine\event\Event;
 use pocketmine\event\Listener;
 use pocketmine\event\world\ChunkLoadEvent;
 use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\world\biome\Biome;
+
 use pocketmine\data\bedrock\BiomeIds;
-use pocketmine\math\Vector3;
+
 use pocketmine\network\mcpe\protocol\LevelEventPacket;
 use pocketmine\network\mcpe\protocol\types\LevelEvent;
 
@@ -18,14 +19,15 @@ class SnowListener implements Listener
      * @param PlayerJoinEvent $event
      */
     public function onJoin(PlayerJoinEvent $event): void
-    {
-        $player = $event->getPlayer();
-        $player->getNetworkSession()->sendDataPacket(LevelEventPacket::create(
-            eventId: LevelEvent::START_RAIN,
-            eventData: 100000,
-            position: null
-        ));
-    }
+	{
+		$player = $event->getPlayer();
+
+		$player->getNetworkSession()->sendDataPacket(LevelEventPacket::create(
+			eventId: LevelEvent::START_RAIN,
+			eventData: 100000,
+			position: null
+		));
+	}
     
     /**
      * @param ChunkLoadEvent $event
@@ -35,7 +37,7 @@ class SnowListener implements Listener
         $chunk = $event->getChunk();
         for ($x = 0; $x < 16; ++$x) {
             for ($z = 0; $z < 16; ++$z) {
-                $chunk->setBiomeId($x, $z, BiomeIds::ICE_PLAINS);
+                $chunk->setBiomeId($x, $chunk->getHighestBlockAt($x, $z), $z, BiomeIds::ICE_PLAINS);
             }
         }
     }
